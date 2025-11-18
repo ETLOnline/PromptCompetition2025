573544570e969d6c104a68f6697724fc5b7f4236
fahamin5149 <fahamin5149@gmail.com>
2025-11-19 01:30:29 +0500

    fixed filter text truncation in participant dashboard

--- app/participant/[competitionId]/page.tsx
+++ app/participant/[competitionId]/page.tsx
@@ -323,7 +323,7 @@ export default function DashboardPage() {
                 </div>
               </div>
               <div className="flex flex-col sm:flex-row gap-4 items-center w-full sm:w-auto">
-                <div className="relative flex-1 max-w-md w-full">
+                <div className="relative flex-1 max-w-sm w-full mr-2">
                   <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
                   <Input
                     placeholder="Search challenges..."
@@ -333,7 +333,7 @@ export default function DashboardPage() {
                   />
                 </div>
                 <Select value={filterSubmissionStatus} onValueChange={(value: any) => setFilterSubmissionStatus(value)}>
-                  <SelectTrigger className="w-40 border-gray-200">
+                  <SelectTrigger className="w-48 border-gray-200">
                     <Filter className="w-4 h-4 mr-2" />
                     <SelectValue />
                   </SelectTrigger>
